89f3e12325113400944f616e057a168671300d53
Tommy Lau <tlau425@yahoo.com>
2019-02-26 22:55:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(array)
   new_array = []
   array.each do |name|
-    new_array.push("Hello, my name is #{name}")
+    new_array.push("Hello, my name is #{name}.")
   end
   return new_array
 end
